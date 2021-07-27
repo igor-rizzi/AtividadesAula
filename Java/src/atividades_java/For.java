628ace337fdb016ed117acdd377bd794d7538968
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-07-27 20:53:25 -0300

    Create For.java

---
+++ Java/src/atividades_java/For.java
@@ -0,0 +1,28 @@
+package atividades_java;
+
+public class For {
+
+/*public static void main(String[] args) {
+		int i;
+		
+		for(i = 120; i <= 280; i++) {
+			System.out.println(i);
+		}
+
+	}*/
+	
+	public static void main(String[] args) {
+		int num, i;
+		num = 0;
+		i = 0;
+		while(num <= 51) {
+			num += i;
+			i++;
+			
+			System.out.println(num);
+		}
+
+	}
+
+
+}
