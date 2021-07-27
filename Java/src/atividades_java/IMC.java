cce24558757a61f9de90ccad23a14b54a719640e
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-07-27 20:53:29 -0300

    Update IMC.java

--- Java/src/atividades_java/IMC.java
+++ Java/src/atividades_java/IMC.java
@@ -1,9 +1,22 @@
 package atividades_java;
 
+import java.util.Scanner;
+
 public class IMC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double IMC, peso, altura;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite a sua altura:");
+		altura = scan.nextDouble();
+		
+		System.out.println("Digite o seu peso:");
+		peso = scan.nextDouble();
+		
+		IMC = peso/(altura*altura);
+		
+		System.out.println("O seu IMC é de: " + IMC);
 
 	}
 
