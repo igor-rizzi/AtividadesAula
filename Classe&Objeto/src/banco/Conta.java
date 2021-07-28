2c8cd1d757a2d2911f75fc89239d0d3d230336a9
Igor Henrique Rizzi <igor.rizzi2@gmail.com>
2021-07-27 22:19:36 -0300

    Create Conta.java

---
+++ Classe&Objeto/src/banco/Conta.java
@@ -0,0 +1,15 @@
+package banco;
+
+public class Conta {
+	int numero;
+	String dono;
+	double saldo;
+
+	void saca(double valor){
+		this.saldo-=valor;
+	}
+	
+	void deposita(double valor){
+		this.saldo += valor;
+	}
+}
