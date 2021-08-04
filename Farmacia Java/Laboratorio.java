ca2c7dc55a00725ef347badb065b35ede3be5892
igor-rizzi <igor.rizzi2@gmail.com>
2021-08-04 08:20:28 -0300

    Create Laboratorio.java

---
+++ Farmacia Java/Laboratorio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Laboratorio{
+    Scanner entrada = new Scanner (System.in);
+    String nomeFantasia;
+    String razaoSocial;
+    String cnpj;
+    String inscricaoEstadual;
+    String endereco;
+    String telefone;
+
+}
