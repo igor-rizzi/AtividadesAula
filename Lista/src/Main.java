f1056460b0b744ba868e5d60b4733cd5c9c8426e
igor-rizzi <igor.rizzi2@gmail.com>
2021-08-11 10:28:58 -0300

    Update Main.java

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -2,20 +2,18 @@ import javax.swing.JOptionPane;
 import java.util.*;
 
 class Main{
-    private static final String String = null;
-
 	public static void main (String[] args){
         Scanner entrada = new Scanner(System.in);
         int opt, list;
         String varDono;
         
-        list = 0;
+        list = 1;
         ArrayList ListaConta = new ArrayList<>();
         
-        JOptionPane.showMessageDialog(null, "Seja Bem-Vindo ao Banco do Ig�o!",null, 1);
+        JOptionPane.showMessageDialog(null, "Seja Bem-Vindo ao Banco do Igão!",null, 1);
         
         do{ 
-		    System.out.println("\nDigite o que deseja realizar na conta\n (0) Criar nova Conta\n (1) Listar contas\n (3) Efetuar Saque\n (4) Efetuar Dep�sito\n (5) Sair\n");
+		    System.out.println("\nDigite o que deseja realizar na conta\n (0) Criar nova Conta\n (1) Listar contas\n (3) Efetuar Saque\n (4) Efetuar Dep�sito\n (5) Sair\n");
 		    opt = entrada.nextInt();
 		   
 			if (opt == 0){
@@ -35,35 +33,36 @@ class Main{
 				int i;
 				for(i = 0; i < ListaConta.size(); i++) {
 					Conta cc = (Conta) ListaConta.get(i);
-					System.out.println("O n�mero da conta �: " + cc.numero);
-					System.out.println("O nome do titular da conta �: " + cc.dono);
-					System.out.println("O saldo da conta �: " + cc.saldo + "\n");
+					JOptionPane.showMessageDialog(null, "O número da conta é: " + cc.numero + "\nO nome do titular da conta é: " + cc.dono + "\nO saldo da conta é: " + cc.saldo, "Conta: " + cc.numero, 1);
 				}
-			}
-			
-			
-			/*else if (opt == 2){
-				System.out.println("\n\nConta do(a): " + minhaConta.dono);
-				System.out.println ("Qual eh o valor do saque\n?");
-				
-				if (conta.saca(entrada.nextDouble())){
-					System.out.println("Saque efetuado!\nNovo Saldo" + minhaConta.saldo);
+			}else if (opt == 2){
+				int i, numUsar;
+				for(i = 0; i < ListaConta.size(); i++) {
+					Conta cc = (Conta) ListaConta.get(i);
+					JOptionPane.showMessageDialog(null, "A conta é!",null, 1);
+					JOptionPane.showMessageDialog(null, "O número da conta é: " + cc.numero + "\nO nome do titular da conta é: " + cc.dono + "\nO saldo da conta é: " + cc.saldo, null, 1);
+					
+					System.out.println("Insira o valor do saque:");
 
-				}else{
-					System.out.println("Saldo insuficiente!\n");
-				}	
+					if (cc.saca(entrada.nextDouble())){
+						System.out.println("Saque efetuado!\nPara a conta do(a): " + cc.dono + "! O saldo foi atualizado para: " + cc.saldo);
+					}else{
+						System.out.println("Saldo insuficiente!\n");
+					}	
+					
+				}
 				
-			}else if(opt == 3){
-				System.out.println("\n\nConta do(a): " + minhaConta.dono);
+			}/*else if(opt == 3){
+				System.out.println("\n\nConta do(a): " + conta1.dono);
 				System.out.println ("Qual eh o valor do deposito\n?");
-				minhaConta.deposita(entrada.nextDouble());
+				Conta.deposita(entrada.nextDouble());
 				
 			}else if (opt == 4){
 				System.out.println("Saiu da aplicacao\n");
 			}else{
 				System.out.println("Opção inválida\n");
 			}*/
-		} while(opt != 4);
+		} while(opt != 5);
 
         
     }
